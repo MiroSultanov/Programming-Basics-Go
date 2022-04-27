7d3ca2cd023edb5ca99653a703b92bccd5b37e2a
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:20:20 +0300

    Update Password Guess.go

--- conditional statement/Password Guess.go
+++ conditional statement/Password Guess.go
@@ -1,3 +1,6 @@
+// Write a program that reads a password (text) entered by the user and checks if the entered password matches the phrase "s3cr3t! P @ ssw0rd". 
+// In case of coincidence, display "Welcome". In case of discrepancy, display "Wrong password!".
+
 package main
 
 import "fmt"
