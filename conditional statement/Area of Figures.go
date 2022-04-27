9ffaaa1bfabc3ce7c8f248c49a8a0eb3eb142ebc
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:13:53 +0300

    Update Area of Figures.go

--- conditional statement/Area of Figures.go
+++ conditional statement/Area of Figures.go
@@ -1,3 +1,11 @@
+// Write a program in which the user enters the type and dimensions of a geometric figure and calculates its face. The figures are of four types: square, square, 
+// rectangle, circle and triangle. The first line of the input reads the type of figure (text with the following options: square, rectangle, circle or triangle).
+// • If the figure is a square: the next line reads a fractional number - the length of its side
+// • If the figure is a rectangle: on the next two lines read two fractional numbers - the lengths of its sides
+// • If the figure is a circle: on the next line reads a fractional number - the radius of the circle
+// • If the figure is a triangle: the next two lines read two fractional numbers - the length of its side and the length of the height to it
+// Round the result to 3 digits after the decimal point.
+
 package main
 
 import "fmt"
