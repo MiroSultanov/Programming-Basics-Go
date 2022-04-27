83626e2c7d65efb3914da5013b783cbb38f74b53
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:23:43 +0300

    Update Sum Seconds.go

--- conditional statement/Sum Seconds.go
+++ conditional statement/Sum Seconds.go
@@ -1,3 +1,6 @@
+// Three athletes finish in a matter of seconds (between 1 and 50). Write a program that reads the times of the competitors in seconds entered by the user and 
+// calculates their total time in the format "minutes: seconds". Display the seconds with leading zero (2 -> "02", 7 -> "07", 35 -> "35").
+
 package main
 
 import "fmt"
