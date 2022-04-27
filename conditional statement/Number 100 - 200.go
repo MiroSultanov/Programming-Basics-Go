1f18707d06841721ed6291453fd0e4309e71f5ca
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:19:33 +0300

    Update Number 100 - 200.go

--- conditional statement/Number 100 - 200.go
+++ conditional statement/Number 100 - 200.go
@@ -1,3 +1,8 @@
+// Write a program that reads an integer entered by the user and checks if it is below 100, between 100 and 200 or above 200. If the number is:
+// • Under 100 print: "Less than 100"
+// • between 100 and 200 print: "Between 100 and 200"
+// • over 200 print: "Greater than 200"
+
 package main
 
 import "fmt"
