f234ada72971cf760043ac0895162a214fd74f28
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:22:11 +0300

    Update Speed Info.go

--- conditional statement/Speed Info.go
+++ conditional statement/Speed Info.go
@@ -1,3 +1,10 @@
+// Write a program that reads the speed (real number) entered by the user and prints speed information.
+// • At speeds up to 10 (inclusive) print "slow"
+// • At speeds above 10 and up to 50 (inclusive) print "average"
+// • At speeds above 50 and up to 150 (inclusive) print "fast"
+// • At speeds above 150 and up to 1000 (inclusive) print "ultra fast"
+// • Print "extremely fast" at higher speeds
+
 package main
 
 import "fmt"
