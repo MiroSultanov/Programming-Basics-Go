b42edcdf65248d209cc62fffcdb36fb7af1b63e4
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:24:33 +0300

    Update Time + 15 Minutes.go

--- conditional statement/Time + 15 Minutes.go
+++ conditional statement/Time + 15 Minutes.go
@@ -1,3 +1,7 @@
+// Write a program that reads the hour and minutes of the 24-hour day entered by the user and calculates what time it will be in 15 minutes. 
+// Print the result in hours: minutes format. The hours are always between 0 and 23, and the minutes are always between 0 and 59. The hours are written in one or two digits.
+// Minutes are always displayed in two digits, with a leading zero when necessary.
+
 package main
 
 import "fmt"
