dcf726066f2df18b0886f00f94233257b059d4e2
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-27 06:18:58 +0300

    Update Lunch Break.go

--- conditional statement/Lunch Break.go
+++ conditional statement/Lunch Break.go
@@ -1,3 +1,20 @@
+// During the lunch break you want to watch an episode of your favorite series. Your task is to write a program that will find out if you have enough time to watch
+// the episode. During the holiday you spend time for lunch and time for rest. Lunch time will be 1/8 of the rest time, and rest time will be 1/4 of the rest time.
+
+// Input
+// 3 lines are read from the console:
+// 1. Name of the series - text
+// 2. Episode duration - integer in the range [10… 90]
+// 3. Duration of the break - an integer in the range [10… 120]
+
+// Output
+// Write one line on the console:
+// • If you have enough time to watch the episode:
+// "You have enough time to watch {series name} and left with {free time} minutes free time."
+// • If you do not have enough time:
+// "You don't have enough time to watch {series name}, you need {time} more minutes."
+// Time to round to the nearest whole number up.
+
 package main
 
 import "fmt"
