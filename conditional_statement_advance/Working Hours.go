c80fd9d4a1c834d3e8ce019575e2591536733394
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-02 15:05:37 +0300

    Update Working Hours.go

--- conditional_statement_advance/Working Hours.go
+++ conditional_statement_advance/Working Hours.go
@@ -1,3 +1,6 @@
+// Write a program that reads an hour of the day (integer) and a day of the week (text) - entered by the user and checks whether the company's office is open, 
+// the office hours are from 10 am to 6 pm, Monday to Saturday including
+
 package main
 
 import "fmt"
