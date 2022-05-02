0b000eaff7c2ae1ffe76248f83574a5b0183dbaf
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-02 14:44:25 +0300

    Update Fruit or Vegetable.go

--- conditional_statement_advance/Fruit or Vegetable.go
+++ conditional_statement_advance/Fruit or Vegetable.go
@@ -1,3 +1,9 @@
+// Write a program that reads a product name entered by the user and checks if it is a fruit or vegetable.
+// • "Fruits" are banana, apple, kiwi, cherry, lemon and grapes
+// • Vegetables are tomato, cucumber, pepper and carrot
+// • All others are "unknown"
+// Display "fruit", "vegetable" or "unknown" according to the entered product.
+
 package main
 
 import "fmt"
