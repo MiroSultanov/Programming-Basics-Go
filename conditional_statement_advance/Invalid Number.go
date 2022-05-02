c4455d9cd305714c655e3018c4457586dd2dbc0e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-02 14:46:51 +0300

    Update Invalid Number.go

--- conditional_statement_advance/Invalid Number.go
+++ conditional_statement_advance/Invalid Number.go
@@ -1,3 +1,6 @@
+// A number is valid if it is in the range [100… 200] or is 0. Write a program that reads an integer entered by the user and print "invalid" 
+// if the number entered is not valid.
+
 package main
 
 import "fmt"
