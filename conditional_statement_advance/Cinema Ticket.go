40e3e85eb962449e2f539e12f943478fbea63af9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-02 14:39:47 +0300

    Update Cinema Ticket.go

--- conditional_statement_advance/Cinema Ticket.go
+++ conditional_statement_advance/Cinema Ticket.go
@@ -1,3 +1,10 @@
+// In a movie theater, the chairs are arranged in a rectangular shape in r rows and c columns. There are three types of screenings with tickets at different prices:
+// • Premiere - premiere screening, at a price of BGN 12.00.
+// • Normal - standard screening, at a price of BGN 7.50.
+// • Discount - screening for children, pupils and students at a reduced price of BGN 5.00.
+// Write a program that reads the type of projection (string), number of rows and number of columns in the hall (integers) entered by the user, and calculates the
+// total ticket revenue for a full hall. Print the result in the format as in the examples below, 2 characters after the decimal point.
+
 package main
 
 import "fmt"
