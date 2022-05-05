ca0192c25b015a7176112660f10f9dd2bef60750
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-05 20:20:21 +0300

    Update Histogram.go

--- for_loops/Histogram.go
+++ for_loops/Histogram.go
@@ -1,3 +1,7 @@
+// N integers are given in the interval [1… 1000]. Of these, some percentage p1 are below 200, another percentage p2 are from 200 to 399, another percentage 
+// p3 are from 400 to 599, another percentage p4 are from 600 to 799 and the remaining p5 percent are from 800 up. 
+// Write a program that calculates and prints the percentages p1, p2, p3, p4 and p5.
+
 package main
 
 import "fmt"
