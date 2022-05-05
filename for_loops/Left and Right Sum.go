6f54274e4951ae3cd12b0033b0ddab98fcbe8779
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-05 20:21:04 +0300

    Update Left and Right Sum.go

--- for_loops/Left and Right Sum.go
+++ for_loops/Left and Right Sum.go
@@ -1,3 +1,7 @@
+// Write a program that reads 2 * n numbers of integers submitted by the user and checks if the sum of the first n numbers (left sum) is equal to the sum of the second
+// n numbers (right sum). In case of equality, print "Yes, sum =" + the amount; otherwise print "No, diff =" + the difference. The difference is calculated as a 
+// positive number (in absolute value).
+
 package main
 
 import "fmt"
