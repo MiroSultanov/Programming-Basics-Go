36cae97c70165c0e7881cf21da44f152a16954d6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-05 20:18:00 +0300

    Update Clever Lily.go

--- for_loops/Clever Lily.go
+++ for_loops/Clever Lily.go
@@ -1,3 +1,11 @@
+// Lily is already N years old. She receives a gift for each of her birthdays.
+// • For odd birthdays (1, 3, 5 ... n) receives toys.
+// • For even birthdays (2, 4, 6 ... n) receives money.
+// For the second birthday he receives BGN 10.00, as the amount increases by BGN 10.00, for each subsequent even birthday (2 -> 10, 4 -> 20, 6 -> 30 ... etc.). 
+// Over the years, Lily has secretly saved money. Lily's brother, in the years she receives money, takes BGN 1.00 from them. Lily sold the toys received over the years, 
+// each for P levs and added the amount to the money saved. With the money she wanted to buy a washing machine for X leva. 
+// Write a program to calculate how much money she has raised and whether she has enough to buy a washing machine.
+
 package main
 
 import (
