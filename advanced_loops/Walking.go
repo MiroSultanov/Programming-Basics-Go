5e90ea7e8e8ef552d687e31db7286abbb54274d2
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-11 06:31:23 +0300

    Update Walking.go

--- advanced_loops/Walking.go
+++ advanced_loops/Walking.go
@@ -1,3 +1,10 @@
+// # Gabby wants to start a healthy lifestyle and has set a goal of walking 10,000 steps every day. 
+// # However, some days she is very tired from work and will want to go home before she achieves her goal. 
+// # Write a program that reads from the console how many steps it takes each time it goes out during the day and when it achieves its goal of writing
+// # "Goal reached! Good job!" and how many more steps she took "{the difference between the steps} steps over the goal!"
+// # If she wants to go home before that, she will enter the "Going home" command and enter the steps she took while returning home. 
+// # Then, if it fails to achieve its goal, the console should read: "{step difference} more steps to reach goal."
+
 package main
 
 import (
