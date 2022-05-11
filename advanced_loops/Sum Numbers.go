e6d8e2037b0cd3ef8b9632ced65d539592b2b6da
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-11 06:30:02 +0300

    Update Sum Numbers.go

--- advanced_loops/Sum Numbers.go
+++ advanced_loops/Sum Numbers.go
@@ -1,3 +1,6 @@
+// # Write a program that reads an integer from the console and integers on each line until their sum is greater than or equal to the original number. 
+// # At the end of the reading, print the sum of the entered numbers.
+
 package main
 
 import "fmt"
