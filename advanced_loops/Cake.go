0d2b8cf273786972b6efa8707317e8588a47f733
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-11 06:21:50 +0300

    Update Cake.go

--- advanced_loops/Cake.go
+++ advanced_loops/Cake.go
@@ -1,3 +1,11 @@
+// # You are invited to the 30th birthday, when the birthday boy eats a huge cake. However, he does not know how many pieces his guests can take from her. 
+// # Your task is to write a program that calculates the number of pieces that guests have taken before it ends. You will get the dimensions of the 
+// # cake (width and length - integers and then on each line, until you receive the command "STOP" or until the cake is finished, the number of 
+// # pieces that guests take from it. Each piece of cake is 1x1 cm .
+// # Print one of the following lines on the console:
+// # "{Number of pieces} pieces are left." - if you reach STOP and the pieces of cake are not finished;
+// #  "No more cake left! You need {number of missing pieces} pieces more."
+
 package main
 
 import (
