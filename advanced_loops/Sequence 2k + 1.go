d2bcbe54e0b7d4a704bdb70a2c0272edcee17a26
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-11 06:29:33 +0300

    Update Sequence 2k + 1.go

--- advanced_loops/Sequence 2k + 1.go
+++ advanced_loops/Sequence 2k + 1.go
@@ -1,3 +1,6 @@
+// # Write a program that reads the number n entered by the user and prints all the numbers ≤ n from the sequence: 1, 3, 7, 15, 31,…. 
+// # Each subsequent number is calculated by multiplying the previous one by 2 and adding 1.
+
 package main
 
 import "fmt"
