eb107518007c90631723c5b9314aeb7748f05e26
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-11 06:24:11 +0300

    Update Exam Preparation.go

--- advanced_loops/Exam Preparation.go
+++ advanced_loops/Exam Preparation.go
@@ -1,3 +1,22 @@
+// # Write a program in which Marin solves exam tasks until he receives an "Enough" message from his lecturer. 
+// # For each task solved, he receives a grade. The program must end reading data with the "Enough" command or if Marin receives the specified number of unsatisfactory
+// # grades. Any grade less than or equal to 4 is unsatisfactory.
+
+// # Input
+// # ⦁ In the first row - number of unsatisfactory grades - integer;
+// # ⦁ Then two lines are read repeatedly:
+// # ⦁ Task name - text;
+// # ⦁ Score - an integer in the range [2… 6]
+
+// # Output
+// # ⦁ If Marin reaches the "Enough" command, print in 3 lines:
+// # ⦁ "Average score:"
+// # ⦁ "Number of problems: {number of all tasks}"
+// # ⦁ "Last problem: {name of last task}"
+// # ⦁ If he receives the specified number of unsatisfactory grades:
+// # } "You need a break, {number of unsatisfactory grades} poor grades."
+// # The average score should be formatted to the second decimal place.
+
 package main
 
 import "fmt"
