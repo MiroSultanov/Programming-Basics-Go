1feef6e12f662c7d7b61fa9db4a665421c8b2f1b
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-11 06:20:52 +0300

    Update AccountBalance.go

--- advanced_loops/AccountBalance.go
+++ advanced_loops/AccountBalance.go
@@ -1,3 +1,9 @@
+// # Write a program that calculates how much money is in the account after you make a certain number of installments. 
+// # On each line you will receive the amount you need to deposit in the account until you receive the command "NoMoreMoney". 
+// # For each amount received, the console must display "Increase:" + the amount and add it to the account. If you get a number less than 0, "Invalid operation!" 
+// # Must be displayed on the console. and the program ends. 
+// # When the program ends, "Total:" must be printed + the total amount in the account formatted to the second decimal place.
+
 package main
 
 import (
