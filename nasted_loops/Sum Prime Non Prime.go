a3d1535442d2b814524a8703ce6586f6bf422c8c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-12 23:05:05 +0300

    Update Sum Prime Non Prime.go

--- nasted_loops/Sum Prime Non Prime.go
+++ nasted_loops/Sum Prime Non Prime.go
@@ -1,3 +1,11 @@
+// # Write a program that reads integers from the console until a "stop" command is received. Find the sum of all entered primes and the sum of all
+// # entered primes. Since, by definition in mathematics, negative numbers cannot be prime, if a negative number is input, the following message
+// # "Number is negative." In this case, the entered number is ignored and is not added to either of the two amounts, and the program continues its execution,
+// # waiting for the next number to be entered.
+// # At the output to print in two lines the two amounts found in the following format:
+// #  "Sum of all prime numbers is: {prime numbers sum}"
+// # ⦁ "Sum of all non prime numbers is: {nonprime numbers sum}"
+
 package main
 
 import (
