d2720b216178a355f23339f6fc916595e4618663
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-12 23:04:03 +0300

    Update Special Numbers.go

--- nasted_loops/Special Numbers.go
+++ nasted_loops/Special Numbers.go
@@ -1,3 +1,18 @@
+// # Write a program that reads an integer N entered by the user and generates all possible "special" numbers from 1111 to 9999. 
+// # To be a "special" number, it must meet the following condition:
+// # ⦁ N to be divisible by each of its digits without remainder.
+// # Example: at N = 16, 2418 is a special number:
+// # ⦁ 16/2 = 8 without residue
+// # ⦁ 16/4 = 4 without residue
+// # ⦁ 16/1 = 16 without residue
+// # ⦁ 16/8 = 2 without residue
+
+// # Input
+// # The input is read from the console and consists of an integer in the range [1… 600000]
+
+// # Output
+// # All "special" numbers must be printed on the console, separated by a space
+
 package main
 
 import "fmt"
