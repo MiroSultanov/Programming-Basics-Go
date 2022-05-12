9fe7dcb94a1343d5f33ff46974aca6a9fbedde60
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-12 23:05:55 +0300

    Update Traveling.go

--- nasted_loops/Traveling.go
+++ nasted_loops/Traveling.go
@@ -1,3 +1,10 @@
+// # Annie loves to travel and wants to visit several different destinations this year. When she chooses a destination, she will decide how much money 
+// # she will need to get there, and she will start saving. When she has saved enough, she will be able to travel.
+// # The destination and the minimum budget (decimal number) that will be needed for the trip will be read from the console each time.
+// # Then a few sums (decimal numbers) will be read, which Annie saves by working and when she manages to collect enough for the trip,
+// # she will leave, and the console should say: "Going to {destination}!"
+// # When she has visited all the destinations she wants, instead of a destination she will enter "End" and the program will end. 
+
 package main
 
 import "fmt"
