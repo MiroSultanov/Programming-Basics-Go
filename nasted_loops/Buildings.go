f6a8779f4d90c3d684ec971db8bc5b19453250a5
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-12 23:01:28 +0300

    Update Buildings.go

--- nasted_loops/Buildings.go
+++ nasted_loops/Buildings.go
@@ -1,3 +1,12 @@
+// # Write a program that displays the numbers of the rooms in a building (in descending order) on the console, provided that the following conditions are met:
+// # ⦁ There are only offices on each even floor;
+// # ⦁ There are only apartments on each odd floor;
+// # ⦁ Each apartment is denoted as follows: A {floor number} {apartment number}, apartment numbers start from 0;
+// # ⦁ Each office is denoted as follows: О {floor number} {office number}, office numbers also start from 0;
+// # ⦁ There are always apartments on the top floor and they are bigger than the others, so their number says 'L' instead of 'A'. 
+// # If there is only one floor, there are only large apartments!
+// # Two integers are read from the console - the number of floors and the number of rooms per floor.
+
 package main
 
 import "fmt"
