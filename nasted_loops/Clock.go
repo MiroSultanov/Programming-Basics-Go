183e55dbd43c0ddb2eb8b1ff8314b45ae4986fd9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-05-12 23:02:27 +0300

    Update Clock.go

--- nasted_loops/Clock.go
+++ nasted_loops/Clock.go
@@ -1,3 +1,5 @@
+// # Write a program that prints the hours of the day from 0: 0 to 23:59, each on a separate line. The hours must be displayed in the format "{hour}: {minutes}".
+
 package main
 
 import "fmt"
